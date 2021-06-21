1269c885ae899667ca5fc6100a37a240161a31b1
Gurudutt-Goswami <86184439+Gurudutt-Goswami@users.noreply.github.com>
2021-06-21 14:57:05 +0530

    Add files via upload

---
+++ Mac Doc/Mac Doc.js
@@ -0,0 +1,58 @@
+$("#img1").hover(function()
+{$(this).animate({width:"70px",height:"70px"});
+	},function()
+{$(this).animate({width:"50px",height:"50px"});});
+
+
+$("#img2").hover(function()
+{$(this).animate({width:"70px",height:"70px"});
+	},function()
+{$(this).animate({width:"50px",height:"50px"});});
+
+
+$("#img3").hover(function()
+{$(this).animate({width:"70px",height:"70px"});
+	},function()
+{$(this).animate({width:"50px",height:"50px"});});
+
+
+$("#img4").hover(function()
+{$(this).animate({width:"70px",height:"70px"});
+	},function()
+{$(this).animate({width:"50px",height:"50px"});});
+
+
+$("#img5").hover(function()
+{$(this).animate({width:"70px",height:"70px"});
+	},function()
+{$(this).animate({width:"50px",height:"50px"});});
+
+
+$("#img6").hover(function()
+{$(this).animate({width:"70px",height:"70px"});
+	},function()
+{$(this).animate({width:"50px",height:"50px"});});
+
+
+$("#img7").hover(function()
+{$(this).animate({width:"70px",height:"70px"});
+	},function()
+{$(this).animate({width:"50px",height:"50px"});});
+
+
+$("#img8").hover(function()
+{$(this).animate({width:"70px",height:"70px"});
+	},function()
+{$(this).animate({width:"50px",height:"50px"});});
+
+
+
+
+$("#simg1").css("transform","scale(1,-1)");
+$("#simg2").css("transform","scale(1,-1)");
+$("#simg3").css("transform","scale(1,-1)");
+$("#simg4").css("transform","scale(1,-1)");
+$("#simg5").css("transform","scale(1,-1)");
+$("#simg6").css("transform","scale(1,-1)");
+$("#simg7").css("transform","scale(1,-1)");
+$("#simg8").css("transform","scale(1,-1)");
