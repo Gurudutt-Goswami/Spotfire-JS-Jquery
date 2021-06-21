d3167a23eadb128e1e0fc9d7cec521f11bc05db2
Gurudutt-Goswami <86184439+Gurudutt-Goswami@users.noreply.github.com>
2021-06-21 14:46:19 +0530

    Add files via upload

---
+++ Left Panel + Accordion/Left Panel + Accordion (Accordion).js
@@ -0,0 +1,9 @@
+$( "#accordion" ).accordion({
+   heightStyle: 'content',
+   collapsible:true,
+   active:false}
+);
+$("body").append("<style>\
+.ui-accordion-header{font-size:18px;font-weight:bold;color:white;font-family:calibri;background:#E60F37}\
+.ui-accordion-content{font-size:10px;background:#E60F37;}\
+</style>")
