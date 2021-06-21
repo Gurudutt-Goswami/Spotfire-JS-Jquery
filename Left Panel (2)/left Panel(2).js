7ad3a5367c61fe9eee965d285a2c6f6a75206a66
Gurudutt-Goswami <86184439+Gurudutt-Goswami@users.noreply.github.com>
2021-06-21 14:28:37 +0530

    Add files via upload

---
+++ Left Panel (2)/left Panel(2).js
@@ -0,0 +1,136 @@
+$("#name").css("transform","rotate(90deg)");
+$("#name1").css("transform","rotate(90deg)");
+
+$("#one").hover(
+function(){
+	$(this).css("background","#166A96");
+	$(this).css("color","white");
+	$("#icon6").css("color","black");
+	},function(){
+	$(this).css("background","#D0ECF0");
+	$(this).css("color","#717199");
+	$("#icon6").css("color","");
+		}	
+)
+
+
+$("#two").hover(
+function(){
+	$(this).css("background","#166A96");
+	$(this).css("color","white");
+	$("#icon7").css("color","black");
+	},function(){
+	$(this).css("background","#D0ECF0");
+	$(this).css("color","#717199");
+	$("#icon7").css("color","");
+		}	
+)
+
+
+$("#three").hover(
+function(){
+	$(this).css("background","#166A96");
+	$(this).css("color","white");
+	$("#icon8").css("color","black");
+	},function(){
+	$(this).css("background","#D0ECF0");
+	$(this).css("color","#717199");
+	$("#icon8").css("color","");
+		}	
+)
+
+
+$("#four").hover(
+function(){
+	$(this).css("background","#166A96");
+	$(this).css("color","white");
+	$("#icon9").css("color","black");
+	},function(){
+	$(this).css("background","#D0ECF0");
+	$(this).css("color","#717199");
+	$("#icon9").css("color","");
+		}	
+)
+
+
+$("#five").hover(
+function(){
+	$(this).css("background","#166A96");
+	$(this).css("color","white");
+	$("#icon1").css("color","black");
+	},function(){
+	$(this).css("background","#D0ECF0");
+	$(this).css("color","#717199");
+	$("#icon1").css("color","");
+		}	
+)
+
+
+$("#six").hover(
+function(){
+	$(this).css("background","#166A96");
+	$(this).css("color","white");
+	$("#icon2").css("color","black");
+	},function(){
+	$(this).css("background","#D0ECF0");
+	$(this).css("color","#717199");
+	$("#icon2").css("color","");
+		}	
+)
+
+
+$("#seven").hover(
+function(){
+	$(this).css("background","#166A96");
+	$(this).css("color","white");
+	$("#icon3").css("color","black");
+	},function(){
+	$(this).css("background","#D0ECF0");
+	$(this).css("color","#717199");
+	$("#icon3").css("color","");
+		}	
+)
+
+
+$("#eight").hover(
+function(){
+	$(this).css("background","#166A96");
+	$(this).css("color","white");
+	$("#icon4").css("color","black");
+	},function(){
+	$(this).css("background","#D0ECF0");
+	$(this).css("color","#717199");
+	$("#icon4").css("color","");
+		}	
+)
+
+
+$("#nine").hover(
+function(){
+	$(this).css("background","#166A96");
+	$(this).css("color","white");
+	$("#icon5").css("color","black");
+	},function(){
+	$(this).css("background","#D0ECF0");
+	$(this).css("color","#717199");
+	$("#icon5").css("color","");
+		}	
+)
+
+
+
+
+var num=0
+$("#dum").click(
+	function(){
+		$("#fun").toggle("slide");	
+		$("#dum").css("display","none");
+		num++;
+		})
+$("#dum1").click(
+		function(){
+		$("#fun").toggle("slide");
+		$("#dum").toggle("slide");
+		num--;
+		}
+		)
