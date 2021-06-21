3d4630071cdfbaffc52acc7e9da063540330196e
Gurudutt-Goswami <86184439+Gurudutt-Goswami@users.noreply.github.com>
2021-06-21 12:45:34 +0530

    Add files via upload

---
+++ Airline Circle/Airline Circle.js
@@ -0,0 +1,90 @@
+$("#first").toggle("slow",function(){
+		$("#kanpur").toggle("slow",function(){
+			$("#firstdash").toggle("slow",function(){
+					$("#firstdash").css("border","5px dashed white");
+					$("#firstdash").animate({width:"100px"});
+					$("#second").toggle("slow",function(){
+						$("#allahabad").toggle("slow",function(){
+							$("#seconddash").css("border","5px dashed white");
+							$("#seconddash").animate({width:"100px"});
+							$("#third").toggle("slow",function(){
+								$("#varanasi").toggle("slow",function(){
+									$("#fourthdash").css("transform","rotate(330deg)");
+									$("#fifthdash").css("transform","rotate(30deg)");
+									$("#fourthdash").css("border","5px dashed white");
+									$("#fourthdash").animate({width:"100px"});
+									$("#fifthdash").css("border","5px dashed white");
+									$("#fifthdash").animate({width:"100px"});
+									$("#fourth").toggle("slow",function(){
+											$("#varanasi1").toggle("slow",function(){});
+									})
+									$("#fifth").toggle("slow",function(){
+											$("#varanasi2").toggle("slow",function(){
+												$("#sixthdash").css("transform","rotate(330deg)");
+												$("#seventhdash").css("transform","rotate(30deg)");
+												$("#eighthdash").css("transform","rotate(330deg)");
+												$("#ninthdash").css("transform","rotate(30deg)");
+												$("#sixthdash").css("border","5px dashed white");
+												$("#sixthdash").animate({width:"100px"});
+												$("#seventhdash").css("border","5px dashed white");
+												$("#seventhdash").animate({width:"100px"});
+												$("#eighthdash").css("border","5px dashed white");
+												$("#eighthdash").animate({width:"100px"});
+												$("#ninthdash").css("border","5px dashed white");
+												$("#ninthdash").animate({width:"100px"});
+												
+												$("#six").toggle("slow",function(){});
+												$("#seven").toggle("slow",function(){});
+												$("#eight").toggle("slow",function(){});
+												$("#nine").toggle("slow",function(){
+													
+													
+													
+												$("#tenthdash").css("transform","rotate(330deg)");
+												$("#eleventhdash").css("transform","rotate(30deg)");
+												$("#twelvedash").css("transform","rotate(330deg)");
+												$("#thirteenthdash").css("transform","rotate(30deg)");
+												$("#fourteenthdash").css("transform","rotate(330deg)");
+												$("#fifteenthdash").css("transform","rotate(30deg)");
+												$("#sixteenthdash").css("transform","rotate(330deg)");
+												$("#seventeenthdash").css("transform","rotate(30deg)");
+												
+												
+												
+												$("#tenthdash").css("border","5px dashed white");
+												$("#tenthdash").animate({width:"100px"});
+												$("#eleventhdash").css("border","5px dashed white");
+												$("#eleventhdash").animate({width:"100px"});
+												$("#twelvedash").css("border","5px dashed white");
+												$("#twelvedash").animate({width:"100px"});
+												$("#thirteenthdash").css("border","5px dashed white");
+												$("#thirteenthdash").animate({width:"100px"});
+												$("#fourteenthdash").css("border","5px dashed white");
+												$("#fourteenthdash").animate({width:"100px"});
+												$("#fifteenthdash").css("border","5px dashed white");
+												$("#fifteenthdash").animate({width:"100px"});
+												$("#sixteenthdash").css("border","5px dashed white");
+												$("#sixteenthdash").animate({width:"100px"});
+												$("#seventeenthdash").css("border","5px dashed white");
+												$("#seventeenthdash").animate({width:"100px"});
+													
+													$("#ten").toggle("slow",function(){});
+													$("#eleven").toggle("slow",function(){});
+													$("#twelve").toggle("slow",function(){});
+													$("#onethree").toggle("slow",function(){});
+													$("#onefour").toggle("slow",function(){});
+													$("#onefive").toggle("slow",function(){});
+													$("#onesix").toggle("slow",function(){});
+													$("#oneseven").toggle("slow",function(){});
+													
+													
+													});
+												});
+										});
+									});
+								});
+							});
+						});
+					})
+		});
+});
