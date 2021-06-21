4a5ef970b109253b25b09a474a723fcaed55a6ce
Gurudutt-Goswami <86184439+Gurudutt-Goswami@users.noreply.github.com>
2021-06-21 13:41:10 +0530

    Add files via upload

---
+++ Left Panel.js
@@ -0,0 +1,47 @@
+$("#triple_line_circle").hover(function(){
+	$(this).css("background","black");
+	$(this).css("border","1px solid white");
+	$("#tl1").css("border-bottom","3px solid black");
+	$("#tl2").css("border-bottom","3px solid black");
+	$("#tl3").css("border-bottom","3px solid black");},
+	function(){
+	$(this).css("background","#DD1338");
+	$(this).css("border","none");
+	$("#tl1").css("border-bottom","3px solid #DD1338");
+	$("#tl2").css("border-bottom","3px solid #DD1338");
+	$("#tl3").css("border-bottom","3px solid #DD1338");});
+
+$("#fun1").hover(function(){
+	$(this).css("background","#DD1338");},
+	function(){
+	$(this).css("background","#F1103A");});
+	
+$("#fun2").hover(function(){
+	$(this).css("background","#DD1338");},
+	function(){
+	$(this).css("background","#F1103A");});
+	
+$("#fun3").hover(function(){
+	$(this).css("background","#DD1338");},
+	function(){
+	$(this).css("background","#F1103A");});
+	
+$("#fun4").hover(function(){
+	$(this).css("background","#DD1338");},
+	function(){
+	$(this).css("background","#F1103A");});
+	
+$("#fun5").hover(function(){
+	$(this).css("background","#DD1338");},
+	function(){
+	$(this).css("background","#F1103A");});
+	
+	
+	
+var num=0;
+if(num==0)
+$("#triple_line_circle").click(function(){
+	$("#table").animate({width:"toggle"});num++;});
+	if(num==1)
+$("#triple_line_circle").click(function(){
+	$("#table").css("display","none");num--;});
